7eb60e06124e2898890daf29076f187d0ed12ad1
Caio Kenedy <caiokenedysantos@gmail.com>
2022-02-23 16:05:50 -0300

    working on JS

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Ativar Links do Menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+console.log(parametros);
