f3c88c02684bea4b0d95d758dbe13dfddd19bf5c
Caio Kenedy <caiok@MacBook-Air-de-Caio.local>
2022-02-23 22:21:25 -0300

    added FAQ and Image gallery with JS

--- js/script.js
+++ js/script.js
@@ -25,3 +25,49 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 console.log(parametros);
+
+// perguntas e respostas
+
+const perguntas = document.querySelectorAll("button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(controls);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// galeria de imagens
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animar
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
